49572516b564630229791f8ab95df27198491231
Samuel Linkon <samuellinkon@gmail.com>
2018-01-31 09:48:10 -0200

    REVISAO3

---
+++ main.js
@@ -0,0 +1,4 @@
+function aparecertxt() 
+{
+	document.getElementById('nome').innerHTML="Desevolvedor junior com experiência em HTML, CSS e JavaScript <br> Conhecimento de linguagem C e JAVA e Banco de Dados";
+}
